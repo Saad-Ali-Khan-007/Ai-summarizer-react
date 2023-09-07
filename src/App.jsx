b1e50e6731c65e77bd167772de2a3e5e02658481
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-09-07 20:42:52 +0500

    AI Summarizer

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Demo from "./components/Demo";
+import Hero from "./components/Hero";
+import "./App.css";
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+        <div className="App">
+          <Hero />
+          <Demo />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
